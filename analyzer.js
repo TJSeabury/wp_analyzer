f1e01737b0ab8d37718e7adcb5e99d99fa9b0a24
TJSeabury <tylerseabury@gmail.com>
2022-04-20 11:25:58 -0400

    cleaned up

--- analyzer.js
+++ analyzer.js
@@ -1,14 +1,8 @@
-import 'dotenv/config';
 import fs from 'fs';
-import fetch from "node-fetch";
 
-
-const {
-    domain,
-    username,
-    password
-} = process.env;
-
+import {
+    exhaustPagedContent
+} from './api';
 
 function writeCSV () {
     let data = 'Old Filename, New Filename\n';
@@ -17,91 +11,6 @@ function writeCSV () {
     }
     fs.writeFileSync( 'filenameMappings.csv', data );
 }
-
-async function API ( domain, userName, password, route = 'wp/v2/' ) {
-    const url = "http://" + domain + "/wp-json/" + route;
-
-    const token = Buffer.from( `${userName}:${password}` ).toString( 'base64' );
-
-    const response = await fetch( url, {
-        method: 'POST',
-        body: JSON.stringify( {
-            username: userName,
-            password: password
-        } ),
-        headers: {
-            'content-type': 'application/json',
-            'authorization': "Basic " + token
-        }
-    } );
-    return response.json();
-}
-
-const hitApi = async ( route, filter = d => d ) => await filter( await API(
-    domain,
-    username,
-    password,
-    route
-) );
-
-function routeWithParams ( route, params ) {
-    let flatParams = '';
-    params = Object.entries( params );
-    let i = 0;
-    for ( const [k, v] of params ) {
-        flatParams += `${k}=${v}${i++ < params.length - 1 ? '&' : ''}`;
-    }
-    const endpoint = `${route}?${flatParams}`;
-    return endpoint;
-}
-
-async function exhaustPagedContent ( route, params, filter = d => d ) {
-    if ( !params || !params?.per_page ) {
-        params = {
-            per_page: 100
-        };
-    }
-    params.offset = 0;
-    let data = [];
-    let temp = null;
-    while ( ( temp = await hitApi( routeWithParams( route, params ), filter ) ).length === params.per_page ) {
-        data = [...data, temp];
-        params.offset = data.length * params.per_page;
-    }
-    data = [...data, temp];
-    return data.reduce( ( accum, page ) => [...accum, ...page], [] );
-}
-
-async function inflateCategory ( id ) {
-    const cat = await hitApi( `/wp/v2/categories/${id}` );
-    return {
-        id: cat.id,
-        name: cat.name,
-        slug: cat.slug,
-    };
-}
-
-async function inflateTag ( id ) {
-    return await hitApi( `/wp/v2/tags/${id}` );
-}
-
-const metaOnlyFilter = async data => Promise.all( data.map( async datum => {
-    const categories = datum.categories !== undefined
-        ? await Promise.all( datum.categories?.map( id => inflateCategory( id ) ) )
-        : null;
-    const tags = await datum.tags?.map( id => inflateTag( id ) );
-    return {
-        id: datum.id,
-        title: datum.title?.rendered,
-        slug: datum.slug,
-        status: datum.status,
-        link: datum.link,
-        published_date: datum.date,
-        modified_date: datum.modified,
-        categories: categories,
-        tags: tags
-    };
-} ) );
 
 const getRouteWriteData = async endpoint => {
     const pp = 100;
